3a02b8ffd6842103eb41bc707c9da60e588591f2
Travis <Tmarndt1991@gmail.com>
2022-02-06 21:56:56 -0500

    Modified index.ts file

--- index.ts
+++ index.ts
@@ -3,4 +3,5 @@ export { IWorkflowBuilder } from "./src/WorkflowBuilder";
 export { WorkflowContext } from "./src/WorkflowContext";
 export { WorkflowStep } from "./src/WorkflowStep";
 export { IWorkflowStepBuilder } from "./src/WorkflowStepBuilder";
-export { IWorkflowStepBuilderFinally } from "./src/WorkflowStepBuilderFinally";
+export { IWorkflowStepBuilderCondition } from "./src/WorkflowStepBuilderCondition"
+export { IWorkflowStepBuilderFinal } from "./src/WorkflowStepBuilderFinal";
